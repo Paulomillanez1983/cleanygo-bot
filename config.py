30d879a5d993f0c2e9791ef1b836ec9e6a5d062e
Paulomillanez1983 <86369587+Paulomillanez1983@users.noreply.github.com>
2026-03-03 15:27:36 -0300

    Update config.py

--- config.py
+++ config.py
@@ -2,14 +2,11 @@ import os
 import logging
 from telebot import TeleBot
 
-# ==================== CONFIGURACIÓN ====================
 TOKEN = os.getenv("BOT_TOKEN", "8534288619:AAG1i5-PdjUABerTQCp_y84XubBfVNJ34FU")
 DB_FILE = "cleanygo_ux.db"
 
-# Inicializar bot
 bot = TeleBot(TOKEN, parse_mode="HTML")
 
-# Logging
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
