dba72c028f20354c81ea41b2ae8c5b288045afcd
Paulomillanez1983 <86369587+Paulomillanez1983@users.noreply.github.com>
2026-03-04 17:12:32 -0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,10 @@
 # wsgi.py
-from bot import app  # importa tu Flask app
-
-# Opcional: logging básico si falla import
 import logging
 logging.basicConfig(level=logging.INFO)
-logging.info("wsgi.py cargado - app importada")
+logger = logging.getLogger(__name__)
+
+logger.info("wsgi.py cargado")
+
+from bot import app
+
+logger.info("Flask app importada correctamente desde bot.py")
