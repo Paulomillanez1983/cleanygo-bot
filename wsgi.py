765dae36184670616eb6968ce0e1a9ed4cc392fc
Paulomillanez1983 <86369587+Paulomillanez1983@users.noreply.github.com>
2026-03-04 22:32:50 -0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,9 @@
-# wsgi.py
 import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 logger.info("wsgi.py cargado")
 
-from main import app
+from bot import app
 
-logger.info("Flask app importada correctamente desde main.py")
+logger.info("Flask app importada correctamente desde bot.py")
