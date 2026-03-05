08081d2b497471a4fa1f91914b14cdfab2830c14
Paulomillanez1983 <86369587+Paulomillanez1983@users.noreply.github.com>
2026-03-04 22:33:22 -0300

    Delete wsgi.py

--- wsgi.py
+++
@@ -1,9 +0,0 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-logger.info("wsgi.py cargado")
-
-from bot import app
-
-logger.info("Flask app importada correctamente desde bot.py")
