536d736da08f7008efac62e19149e0b7674ead0f
Paulomillanez1983 <86369587+Paulomillanez1983@users.noreply.github.com>
2026-03-09 20:51:51 -0300

    Fix import statements in callbacks.py
    Updated import statements to correct module paths.

--- handlers/client/callbacks.py
+++ handlers/client/callbacks.py
@@ -8,7 +8,9 @@ from telebot import types, apihelper
 
 # CAMBIO: usar get_bot
 from config import logger, get_bot
-from models.user_state import set_state, update_data, get_data, clear_state, UserState
+# CORREGIDO: Importar funciones desde config y UserState desde models.states
+from config import set_state, update_data, get_data, clear_state
+from models.states import UserState
 from models.services_data import SERVICES
 from utils.icons import Icons
 from utils.keyboards import get_alternative_times_keyboard, get_role_keyboard
