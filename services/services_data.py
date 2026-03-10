bcbf002b797afeb3fe493353d226a3ebab070153
Paulomillanez1983 <86369587+Paulomillanez1983@users.noreply.github.com>
2026-03-10 16:54:33 -0300

    Create services_data.py

---
+++ services/services_data.py
@@ -0,0 +1,28 @@
+from utils.icons import Icons
+
+SERVICES = {
+    "niñera": {
+        "name": "Niñera", 
+        "icon": Icons.BABY, 
+        "desc": "Cuidado de niños",
+        "base_price": 15000
+    },
+    "cuidado": {
+        "name": "Cuidado de personas", 
+        "icon": Icons.ELDER, 
+        "desc": "Adultos mayores",
+        "base_price": 18000
+    },
+    "ac_inst": {
+        "name": "Instalación A/C", 
+        "icon": Icons.SNOW, 
+        "desc": "Aire acondicionado",
+        "base_price": 25000
+    },
+    "ac_tech": {
+        "name": "Visita técnica A/C", 
+        "icon": Icons.TOOLS, 
+        "desc": "Reparación/mantenimiento",
+        "base_price": 20000
+    }
+}
