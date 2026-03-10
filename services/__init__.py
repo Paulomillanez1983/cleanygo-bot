e62ff91cd99a2a2ba106e004cc2265ae6a9876ee
Paulomillanez1983 <86369587+Paulomillanez1983@users.noreply.github.com>
2026-03-10 16:57:50 -0300

    Update __init__.py

--- services/__init__.py
+++ services/__init__.py
@@ -1,1 +1,13 @@
+"""
+Services package
+"""
+from services.request_service import create_request, get_request, update_request_status, assign_worker_to_request_safe
+from services.worker_service import find_available_workers
 
+__all__ = [
+    'create_request', 
+    'get_request', 
+    'update_request_status', 
+    'assign_worker_to_request_safe',
+    'find_available_workers'
+]
