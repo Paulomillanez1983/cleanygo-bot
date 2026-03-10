c9d426a3b6aebbddb3cf98c4bbe41b888610334a
Paulomillanez1983 <86369587+Paulomillanez1983@users.noreply.github.com>
2026-03-10 17:02:28 -0300

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,30 +2,8 @@
 Utils package
 """
 from utils.icons import Icons
-from utils.keyboards import (
-    get_role_keyboard,
-    get_cancel_keyboard,
-    get_location_keyboard,
-    get_service_selector,
-    get_time_selector,
-    get_confirmation_keyboard,
-    get_worker_request_keyboard,
-    get_alternative_times_keyboard
-)
-from utils.telegram_safe import send_safe, edit_safe, delete_safe, answer_callback_safe
+# REMOVIDO: from utils.keyboards import (...)
+# REMOVIDO: from utils.telegram_safe import (...)
 
-__all__ = [
-    'Icons',
-    'get_role_keyboard',
-    'get_cancel_keyboard',
-    'get_location_keyboard',
-    'get_service_selector',
-    'get_time_selector',
-    'get_confirmation_keyboard',
-    'get_worker_request_keyboard',
-    'get_alternative_times_keyboard',
-    'send_safe',
-    'edit_safe',
-    'delete_safe',
-    'answer_callback_safe'
-]
+# Solo exportar Icons, el resto se importa directamente donde se necesita
+__all__ = ['Icons']
